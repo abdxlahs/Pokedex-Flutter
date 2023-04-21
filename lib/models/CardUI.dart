4ebd132fdff04e16776f5753b7ba554c7729f551
abdxlahs <au060134@gmail.com>
2023-04-21 15:25:50 +0500

    latest 21April,2022

---
+++ lib/models/CardUI.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/models/database_helper.dart';
+
+class MyPokemonCard extends StatefulWidget {
+  final String name;
+  final String id;
+  final String imageUrl;
+  final int type;
+  final int type2;
+
+  MyPokemonCard({required this.name, required this.id, required this.imageUrl, required this.type, required this.type2 });
+
+  @override
+  State<MyPokemonCard> createState() => _MyPokemonCardState();
+}
+
+class _MyPokemonCardState extends State<MyPokemonCard> {
+  late Future<String> typeName;
+  late Future<String> type2Name;
+
+  @override
+  void initState() {
+    super.initState();
+    typeName = _getType() ;
+    type2Name= _getsecondType();
+    
+  }
+ 
+  Future<String> _getType() async {
+    var temp = await DatabaseHelper.instance.customQueryWithArgs("select name from Types where id = ?", [widget.type]);
+    return temp[0]["name"].toString();
+  }
+
+  Future<String> _getsecondType() async {
+    if(widget.type2!=0){
+    var temp = await DatabaseHelper.instance.customQueryWithArgs("select name from Types where id = ?", [widget.type2]);
+    return temp[0]["name"].toString();
+    }
+    return '0';
+  }
+
+
+
+  Color _getAvatarColor(String typeName) {
+    switch (typeName) {
+      case 'fire':
+      return Color(0xFFFF5722-0x2);
+    case 'water':
+      return Color(0xFF2196F3);
+    case 'grass':
+      return Color(0xFF4CAF50);
+    case 'electric':
+      return Color(0xFFFFEB3B);
+    case 'psychic':
+      return Color(0xFFF44336);
+    case 'dragon':
+      return Color(0xFF673AB7);
+    case 'normal':
+      return Color(0xFF9E9E9E);
+    case 'fighting':
+      return Color(0xFFD84315);
+    case 'flying':
+      return Color(0xFF90CAF9);
+    case 'poison':
+      return Color(0xFF9C27B0);
+    case 'ground':
+      return Color(0xFF795548);
+    case 'rock':
+      return Color(0xFF607D8B);
+    case 'bug':
+      return Color(0xFF8BC34A);
+    case 'ghost':
+      return Color(0xFF7B1FA2);
+    case 'steel':
+      return Color(0xFF9E9E9E);
+    case 'fairy':
+      return Color(0xFFF48FB1);
+    case 'dark':
+      return Color(0xFF212121);
+    case 'ice':
+      return Color(0xFF80D8FF);
+    default:
+      return Color(0xFFFFCB05);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  return FutureBuilder<List<String>>(
+    future: Future.wait([typeName,type2Name]),
+    builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+      if (snapshot.connectionState == ConnectionState.done) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        final typeName = snapshot.data![0];
+        final type2Name = snapshot.data![1];
+        return Container(
+          width: double.infinity,
+          height: 300,
+          child:Card(
+          color: (_getAvatarColor(typeName)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          elevation: 5.0,
+          child: Padding(
+            padding: EdgeInsets.all(5),
+            child: 
+            Column(
+              children: 
+      [
+                Align(
+                alignment:Alignment.topLeft,
+               child: Text(
+                  '#${widget.id}',
+                  style: TextStyle(fontSize: 20.0,color: Colors.white),
+                )
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:0.5),
+                    child: CircleAvatar(
+                    backgroundColor: _getAvatarColor(typeName),
+                    backgroundImage: NetworkImage(widget.imageUrl),
+                    radius: 75,
+                ),
+                ),
+                Padding(
+                padding:EdgeInsets.only(top:15),
+                 child: Text(
+                  '${widget.name.toUpperCase()}',
+                  style: TextStyle(fontSize: 20.0,color: Colors.white,fontWeight: FontWeight.bold),
+                )),
+                SizedBox(height: 20.0),
+                Padding(
+                  padding: EdgeInsets.only(left: 50),
+                  child: Row(children: [
+                
+                    Container(
+                    padding:type2Name!='0'? EdgeInsets.symmetric(horizontal: 40.0, vertical: 0.1):EdgeInsets.symmetric(horizontal: 120.0, vertical: 0.1),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                      color:  Colors.white,
+                      width: 2.0,
+                      ),
+                      color: _getAvatarColor(typeName), // Change this to the background color you want
+                      borderRadius: BorderRadius.circular(12.0),),
+                     
+                    child : Text(
+                    (typeName ),
+                    style: TextStyle(fontSize: 16.0, color: Colors.white),
+                  ),),
+                
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Container(
+                      padding: type2Name!='0'? EdgeInsets.symmetric(horizontal: 40.0, vertical: 0.1):EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                        color: type2Name!='0'? Colors.white : _getAvatarColor(typeName),
+                        width: 2.0,
+                        ),
+                        color: _getAvatarColor(typeName), // Change this to the background color you want
+                        borderRadius: BorderRadius.circular(12.0),),
+                       
+                      child : Text(
+                      (type2Name!='0'?type2Name:'' ),
+                      style: TextStyle(fontSize: 16.0, color: Colors.white),
+                                      ),),
+                    ),
+                  ],),
+                )
+                      
+            ]
+            ),
+          ),
+        )
+        );
+      } else {
+        return CircularProgressIndicator();
+      }
+    },
+  );
+}
+}
---
+++ lib/models/database_helper.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final _dbName = 'MyDatabase.db3';
+  static final _dbVersion = 1;
+  String _tableName = '';
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    return _database ??= await _initDatabase();
+  }
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _dbName);
+    await _copyDbFromAssets(path);
+
+    return await openDatabase(
+      path,
+      version: _dbVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _copyDbFromAssets(String path) async {
+    ByteData data = await rootBundle.load(join('assets', _dbName));
+    List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    await File(path).writeAsBytes(bytes, flush: true);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {}
+
+  Future<List<Map<String, dynamic>>> fetchData(_tableName) async {
+    Database db = await instance.database;
+    return await db.query(_tableName);
+  }
+
+  //custom query
+   Future<List<Map<String, dynamic>>> customQuery(String query) async {
+    Database db = await instance.database;
+    return await db.rawQuery(query);
+  }
+
+
+  // Update the return type of customQueryWithArgs function
+  Future<List<Map<String, dynamic>>> customQueryWithArgs(String query, List<dynamic> args) async {
+    Database db = await instance.database;
+    return await db.rawQuery(query, args);
+  }
+
+
+}
