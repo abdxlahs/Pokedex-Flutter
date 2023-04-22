6e5e9cb0feedfcc49437ef242f4d8777e1dbc970
abdxlahs <au060134@gmail.com>
2023-04-23 02:14:08 +0500

    April 23,2023

--- lib/pages/home/home_widget.dart
+++ lib/pages/home/home_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -10,7 +11,6 @@ import 'package:provider/provider.dart';
 import 'package:pokedex/models/database_helper.dart';
 import 'package:pokedex/models/CardUI.dart';
 
-
 class HomeWidget extends StatefulWidget {
   const HomeWidget({Key? key}) : super(key: key);
 
@@ -22,15 +22,18 @@ class _HomeWidgetState extends State<HomeWidget> {
   late Future<List<Map<String, dynamic>>> _data;
   late Future<List<Map<String, dynamic>>> _types;
   late Future<List<Map<String, dynamic>>> _custom;
-  
+  final TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
+
   @override
   void initState() {
     super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
     _data = DatabaseHelper.instance.customQuery('SELECT * FROM Pokemon');
     _types = DatabaseHelper.instance.customQuery('SELECT * FROM Types');
-
-    
-                                                        
   }
 
   @override
@@ -38,39 +41,102 @@ class _HomeWidgetState extends State<HomeWidget> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text("Pokèdex"),
+        title: const Text("Pokèdex"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              _searchQuery = _searchController.text;
+              setState(() {
+                _fetchData();
+              });
+            },
+          ),
+        ],
       ),
-      body: FutureBuilder<List<Map<String, dynamic>>> (
-        future: _data,
-        builder: (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
-            final data = snapshot.data!;
-            return ListView.builder (
-              itemCount: data.length,
-              itemBuilder: (BuildContext context, int index) {
-                var type2=data[index]['type2'];
-                print(type2);
-                if(type2 == null ){
-                  type2 = 0;
+      body: Column(
+        children: [
+          Padding(
+  padding: const EdgeInsets.all(8.0),
+  child: Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(32),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          spreadRadius: 1,
+          blurRadius: 5,
+          offset: Offset(0, 2),
+        ),
+      ],
+    ),
+    child: TextField(
+      controller: _searchController,
+      decoration: InputDecoration(
+        hintText: 'Search Pokèmon by name',
+        hintStyle: TextStyle(color: Colors.grey),
+        contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: IconButton(
+          onPressed: () {
+            _searchController.clear();
+            setState(() {
+              _searchQuery = '';
+              _fetchData();
+            });
+          },
+          icon: Icon(Icons.clear),
+          color: Colors.grey,
+        ),
+      ),
+    ),
+  ),
+),
+          Expanded(
+            child: FutureBuilder<List<Map<String, dynamic>>>(
+              future: _data,
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  final data = snapshot.data!;
+                  final filteredData = data
+                      .where((pokemon) =>
+                          pokemon['name']
+                              .toString()
+                              .toLowerCase()
+                              .contains(_searchQuery.toLowerCase()))
+                      .toList();
+                  return ListView.builder(
+                    itemCount: filteredData.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var type2 = filteredData[index]['type2'];
+                      print(type2);
+                      if (type2 == null) {
+                        type2 = 0;
+                      }
+                      return MyPokemonCard(
+                        name: filteredData[index]['name'].toString(),
+                        id: filteredData[index]['id'].toString(),
+                        imageUrl: filteredData[index]['image_url'].toString(),
+                        type: filteredData[index]['type1'],
+                        type2: type2,
+                      );
+                    },
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
                 }
-                return MyPokemonCard (
-                  name: data[index]['name'].toString(),
-                  id: data[index]['id'].toString(),
-                  imageUrl:data[index]['image_url'].toString(),
-                  type: data[index]['type1'],
-                  type2: type2
-                  // Replace 'columnName' with the actual column name from your table
-                );
               },
-              
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
